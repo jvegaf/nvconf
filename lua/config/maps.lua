7f452981e9232512f64a51ab036e15c046f402be
Jose Vega <jevf@gft.com>
2023-03-01 18:23:31 +0100

    maps

--- lua/config/maps.lua
+++ lua/config/maps.lua
@@ -1,15 +1,15 @@
-local opts = { noremap = true, silent = true }
+local NS = { noremap = true, silent = true }
 
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
 
 --Remap space as leader key
-keymap("n", "<Space>", "", opts)
+keymap("n", "<Space>", "", NS)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-keymap("n", "<C-i>", "<C-i>", opts)
+keymap("n", "<C-i>", "<C-i>", NS)
 
-keymap("n", "x", '"_x', opts)
+keymap("n", "x", '"_x', NS)
 
 -- Modes
 --   normal_mode = "n",
@@ -21,72 +21,74 @@ keymap("n", "x", '"_x', opts)
 
 -- Normal --
 -- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
--- keymap('n', '<m-tab>', '<c-6>', opts)
+keymap("n", "<C-h>", "<C-w>h", NS)
+keymap("n", "<C-j>", "<C-w>j", NS)
+keymap("n", "<C-k>", "<C-w>k", NS)
+keymap("n", "<C-l>", "<C-w>l", NS)
+-- keymap('n', '<m-tab>', '<c-6>', NS)
 
 
 -- Cancel search highlighting with ESC
-keymap("n", "<ESC><ESC>", ":nohlsearch<Bar>:echo<CR>", opts)
+keymap("n", "<ESC><ESC>", ":nohlsearch<Bar>:echo<CR>", NS)
 
 -- Visual --
 -- Stay in indent mode
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
+keymap("v", "<", "<gv", NS)
+keymap("v", ">", ">gv", NS)
 
 -- Select all
-keymap("n", "<C-a>", "gg<S-v>G", opts)
+keymap("n", "<C-a>", "gg<S-v>G", NS)
 
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+keymap("n", "<C-Up>", ":resize -2<CR>", NS)
+keymap("n", "<C-Down>", ":resize +2<CR>", NS)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", NS)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", NS)
 
 -- Move Lines
-keymap("n", "<A-Down>", ":m .+1<CR>==", opts)
-keymap("v", "<A-Down>", ":m '>+1<CR>gv=gv", opts)
-keymap("i", "<A-Down>", "<Esc>:m .+1<CR>==gi", opts)
-keymap("n", "<A-Up>", ":m .-2<CR>==", opts)
-keymap("v", "<A-Up>", ":m '<-2<CR>gv=gv", opts)
-keymap("i", "<A-Up>", "<Esc>:m .-2<CR>==gi", opts)
+keymap("n", "<A-Down>", ":m .+1<CR>==", NS)
+keymap("v", "<A-Down>", ":m '>+1<CR>gv=gv", NS)
+keymap("i", "<A-Down>", "<Esc>:m .+1<CR>==gi", NS)
+keymap("n", "<A-Up>", ":m .-2<CR>==", NS)
+keymap("v", "<A-Up>", ":m '<-2<CR>gv=gv", NS)
+keymap("i", "<A-Up>", "<Esc>:m .-2<CR>==gi", NS)
 
-keymap("n","vv", "V", opts)
+keymap("n","vv", "V", NS)
+keymap("n","<leader>o", "o<Esc>", NS)
+keymap("n","<leader>O", "O<Esc>", NS)
 
 -- dont yank on visual paste
-keymap("v", "p", '"_dP', opts)
+keymap("v", "p", '"_dP', NS)
 
-keymap("n", "<F1>", "<cmd>vertical help<cr>", opts)
-keymap("n", "<F4>", "<cmd>Telescope resume<cr>", opts)
-keymap("n", "<F5>", "<cmd>Telescope commands<CR>", opts)
+keymap("n", "<F1>", "<cmd>vertical help<cr>", NS)
+keymap("n", "<F4>", "<cmd>Telescope resume<cr>", NS)
+keymap("n", "<F5>", "<cmd>Telescope commands<CR>", NS)
 keymap(
   "n",
   "<F6>",
   [[:echo "hi<" . synIDattr(synID(line("."),col("."),1),"name") . '> trans<' . synIDattr(synID(line("."),col("."),0),"name") . "> lo<" . synIDattr(synIDtrans(synID(line("."),col("."),1)),"name") . ">" . " FG:" . synIDattr(synIDtrans(synID(line("."),col("."),1)),"fg#")<CR>]],
-  opts
+  NS
 )
-keymap("n", "<F7>", "<cmd>TSHighlightCapturesUnderCursor<cr>", opts)
-keymap("n", "<F8>", "<cmd>TSPlaygroundToggle<cr>", opts)
-keymap("n", "<F11>", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-keymap("n", "<F12>", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-keymap("v", "//", [[y/\V<C-R>=escape(@",'/\')<CR><CR>]], opts) -- search for visualy selected text
-keymap("n", "<C-p>", "<cmd>Telescope command_center<cr>", opts)
-keymap("n", "<C-t>", "<cmd>lua vim.lsp.buf.document_symbol()<cr>", opts)
+keymap("n", "<F7>", "<cmd>TSHighlightCapturesUnderCursor<cr>", NS)
+keymap("n", "<F8>", "<cmd>TSPlaygroundToggle<cr>", NS)
+keymap("n", "<F11>", "<cmd>lua vim.lsp.buf.references()<CR>", NS)
+keymap("n", "<F12>", "<cmd>lua vim.lsp.buf.definition()<CR>", NS)
+keymap("v", "//", [[y/\V<C-R>=escape(@",'/\')<CR><CR>]], NS) -- search for visualy selected text
+keymap("n", "<C-p>", "<cmd>Telescope command_center<cr>", NS)
+keymap("n", "<C-t>", "<cmd>lua vim.lsp.buf.document_symbol()<cr>", NS)
 
-keymap("n", "y", "<Plug>(YankyYank)", opts)
-keymap("x", "y", "<Plug>(YankyYank)", opts)
+keymap("n", "y", "<Plug>(YankyYank)", NS)
+keymap("x", "y", "<Plug>(YankyYank)", NS)
 
 
 -- Comment
-keymap("n", "<m-/>", "<cmd>lua require('Comment.api').toggle_current_linewise()<CR>", opts)
-keymap("x", "<m-/>", '<ESC><CMD>lua require("Comment.api").toggle_linewise_op(vim.fn.visualmode())<CR>', opts)
+keymap("n", "<m-/>", "<cmd>lua require('Comment.api').toggle_current_linewise()<CR>", NS)
+keymap("x", "<m-/>", '<ESC><CMD>lua require("Comment.api").toggle_linewise_op(vim.fn.visualmode())<CR>', NS)
 
 -- Tabs
-keymap("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>", opts)
-keymap("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>", opts)
-keymap("n", "<S-q>", "<cmd>Bdelete<CR>", opts)
+keymap("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>", NS)
+keymap("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>", NS)
+keymap("n", "<S-q>", "<cmd>Bdelete<CR>", NS)
 
 keymap("n", "<leader>x", "<cmd>qall<CR>", {desc = "Exit" ,  noremap = true, silent = true })   
 keymap("n", "<leader>w", "<cmd>w<CR>", {desc = "Write" ,  noremap = true, silent = true })
