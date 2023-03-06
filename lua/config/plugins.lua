40ee402a0a1adc51c2e138b31b75689339830521
Jose Vega <jevf@gft.com>
2023-03-06 23:50:50 +0100

    null-ls

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -123,23 +123,7 @@ return {
     event = "BufReadPre",
     dependencies = { "mason.nvim" },
     config = function()
-      local null_ls = require "null-ls"
-      null_ls.setup {
-        sources = {
-          null_ls.builtins.formatting.stylua,
-          null_ls.builtins.formatting.xmlformat,
-          null_ls.builtins.formatting.prettier,
-
-          null_ls.builtins.diagnostics.eslint_d.with {
-            diagnostics_format = "[eslint] #{m}\n(#{c})",
-          },
-          -- b.diagnostics.php,
-          null_ls.builtins.diagnostics.shellcheck,
-          null_ls.builtins.diagnostics.stylelint,
-          null_ls.builtins.diagnostics.yamllint,
-          -- null_ls.builtins.diagnostics.luacheck,
-        },
-      }
+      require("plugins.null_ls")
     end,
   },
 
