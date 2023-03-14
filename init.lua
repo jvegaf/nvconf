dad5c10a4db4d95da772ab1405cae8098212e31c
Jose Vega <josevega234@gmail.com>
2023-03-14 21:44:54 +0100

    renaming

--- init.lua
+++ init.lua
@@ -1,14 +1,14 @@
-require('jvegaf.base')
-require('jvegaf.highlights')
-require('jvegaf.maps')
-require('jvegaf.plugins')
-require('jvegaf.settings')
+require 'config.base'
+require 'config.highlights'
+require 'config.maps'
+require 'config.plugins'
+require 'config.settings'
 
 local host = vim.loop.os_uname().sysname
 
 if host == 'Linux' then
-  require('jvegaf.linux')
+  require 'config.linux'
   return
 end
 
-require('jvegaf.windows')
+require 'config.windows'
