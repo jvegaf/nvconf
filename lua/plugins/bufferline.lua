3512e8aea9dbc146b25714725ac00e59c7f9fd05
Jose Vega <jevf@gft.com>
2023-04-21 11:47:12 +0200

    bufferline refactor

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -16,17 +16,6 @@ require("bufferline").setup {
     close_icon = "",
     left_trunc_marker = "",
     right_trunc_marker = "",
-    --- name_formatter can be used to change the buffer's label in the bufferline.
-    --- Please note some names can/will break the
-    --- bufferline so use this at your discretion knowing that it has
-    --- some limitations that will *NOT* be fixed.
-    -- name_formatter = function(buf)  -- buf contains:
-    -- name                | str        | the basename of the active file
-    -- path                | str        | the full path of the active file
-    -- bufnr (buffer only) | int        | the number of the active buffer
-    -- buffers (tabs only) | table(int) | the numbers of the buffers in the tab
-    -- tabnr (tabs only)   | int        | the "handle" of the tab, can be converted to its ordinal number using: `vim.api.nvim_tabpage_get_number(buf.tabnr)`
-    -- end,
     max_name_length = 18,
     max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
     truncate_names = true, -- whether or not tab names should be truncated
@@ -68,7 +57,6 @@ require("bufferline").setup {
     color_icons = true, -- whether or not to add the filetype icon highlights
     show_buffer_icons = true, -- disable filetype icons for buffers
     show_buffer_close_icons = true,
-    show_buffer_default_icon = true, -- whether or not an unrecognised filetype should show a default icon
     show_close_icon = true,
     show_tab_indicators = true,
     show_duplicate_prefix = true, -- whether to show duplicate buffer prefix
