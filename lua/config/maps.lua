e43124e119f34d3350220ca7ab0e8f11ccce204c
Jose Vega <josevega234@gmail.com>
2023-06-11 02:19:44 +0200

    neotree

--- lua/config/maps.lua
+++ lua/config/maps.lua
@@ -4,15 +4,15 @@ local opts = { noremap = true, silent = true }
 local term_opts = { silent = true }
 
 -- Shorten function name
-local keymap = vim.api.nvim_set_keymap
+local map = vim.keymap.set
 
 --Remap space as leader key
-keymap('n', '<Space>', '', opts)
+map('n', '<Space>', '', opts)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-keymap('n', '<C-i>', '<C-i>', opts)
+map('n', '<C-i>', '<C-i>', opts)
 
-keymap('n', 'x', '"_x', opts)
+map('n', 'x', '"_x', opts)
 
 -- Modes
 --   normal_mode = "n",
@@ -24,67 +24,59 @@ keymap('n', 'x', '"_x', opts)
 
 -- Normal --
 -- Better window navigation
-keymap('n', '<m-h>', '<C-w>h', opts)
-keymap('n', '<m-j>', '<C-w>j', opts)
-keymap('n', '<m-k>', '<C-w>k', opts)
-keymap('n', '<m-l>', '<C-w>l', opts)
-keymap('n', '<m-tab>', '<c-6>', opts)
+map('n', '<C-h>', '<C-w>h', opts)
+map('n', '<C-j>', '<C-w>j', opts)
+map('n', '<C-k>', '<C-w>k', opts)
+map('n', '<C-l>', '<C-w>l', opts)
+map('n', '<C-tab>', '<c-6>', opts)
 
 -- Cancel search highlighting with ESC
-keymap('n', '<ESC>', ':nohlsearch<Bar>:echo<CR>', opts)
+map('n', '<ESC>', ':nohlsearch<Bar>:echo<CR>', opts)
 
-keymap('n', '<leader>o', 'o<Esc>', opts)
-keymap('n', '<leader>O', 'O<Esc>', opts)
-keymap("n", "<leader>p", "<cmd>pu<cr>", opts)
-keymap("n", "<leader>P", "<cmd>pu!<cr>", opts)
+map("n", "<leader>p", "<cmd>pu<cr>", opts)
+map("n", "<leader>P", "<cmd>pu!<cr>", opts)
 -- Visual --
 -- Stay in indent mode
-keymap('v', '<', '<gv', opts)
-keymap('v', '>', '>gv', opts)
+map('v', '<', '<gv', opts)
+map('v', '>', '>gv', opts)
 
 -- Select all
-keymap('n', '<C-a>', 'gg<S-v>G', opts)
+map('n', '<C-a>', 'gg<S-v>G', opts)
 
 -- Resize with arrows
-keymap('n', '<C-Up>', ':resize -2<CR>', opts)
-keymap('n', '<C-Down>', ':resize +2<CR>', opts)
-keymap('n', '<C-Left>', ':vertical resize -2<CR>', opts)
-keymap('n', '<C-Right>', ':vertical resize +2<CR>', opts)
+map('n', '<C-Left>', ':vertical resize -2<CR>', opts)
+map('n', '<C-Down>', ':resize +2<CR>', opts)
+map('n', '<C-Up>', ':resize -2<CR>', opts)
+map('n', '<C-Right>', ':vertical resize +2<CR>', opts)
 
--- File Browser
-keymap('n', '<A-1>', ':NvimTreeToggle<CR>', opts)
-keymap('n', '<leader>e', ':NvimTreeFocus<CR>', opts)
+-- Move Lines
+map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
 
--- Move text up and down
-keymap('n', '<A-Up>', ':MoveLine(-1)<CR>', opts)
-keymap('n', '<A-Down>', ':MoveLine(1)<CR>', opts)
-keymap('n', '<A-Left>', ':MoveHChar(-1)<CR>', opts)
-keymap('n', '<A-Right>', ':MoveHChar(1)<CR>', opts)
-
-keymap('v', '<A-Up>', ':MoveBlock(-1)<CR>', opts)
-keymap('v', '<A-Down>', ':MoveBlock(1)<CR>', opts)
-keymap('v', '<A-Left>', ':MoveHBlock(-1)<CR>', opts)
-keymap('v', '<A-Right>', ':MoveHBlock(1)<CR>', opts)
-
-keymap('n','vv','V', opts)
+map('n','vv','V', opts)
+map('n','zz', '<cmd>wq<cr>', opts)
 -- dont yank on visual paste
-keymap('v', 'p', '"_dP', opts)
+map('v', 'p', '"_dP', opts)
 
-keymap('n', '<F4>', '<cmd>Telescope resume<cr>', opts)
-keymap('n', '<F5>', '<cmd>Telescope commands<CR>', opts)
-keymap(
+map('n', '<F4>', '<cmd>Telescope resume<cr>', opts)
+map('n', '<F5>', '<cmd>Telescope commands<CR>', opts)
+map(
   'n',
   '<F6>',
   [[:echo "hi<" . synIDattr(synID(line("."),col("."),1),"name") . '> trans<' . synIDattr(synID(line("."),col("."),0),"name") . "> lo<" . synIDattr(synIDtrans(synID(line("."),col("."),1)),"name") . ">" . " FG:" . synIDattr(synIDtrans(synID(line("."),col("."),1)),"fg#")<CR>]],
   opts
 )
-keymap('n', '<F7>', '<cmd>TSHighlightCapturesUnderCursor<cr>', opts)
-keymap('n', '<F8>', '<cmd>TSPlaygroundToggle<cr>', opts)
-keymap('n', '<F11>', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-keymap('n', '<F12>', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-keymap('v', '//', [[y/\V<C-R>=escape(@",'/\')<CR><CR>]], opts)
-keymap('n', '<C-p>', '<cmd>Telescope projects<cr>', opts)
-keymap('n', '<C-t>', '<cmd>lua vim.lsp.buf.document_symbol()<cr>', opts)
+map('n', '<F7>', '<cmd>TSHighlightCapturesUnderCursor<cr>', opts)
+map('n', '<F8>', '<cmd>TSPlaygroundToggle<cr>', opts)
+map('n', '<F11>', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+map('n', '<F12>', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+map('v', '//', [[y/\V<C-R>=escape(@",'/\')<CR><CR>]], opts)
+map('n', '<C-p>', '<cmd>Telescope projects<cr>', opts)
+map('n', '<C-t>', '<cmd>lua vim.lsp.buf.document_symbol()<cr>', opts)
 
 M.show_documentation = function()
   local filetype = vim.bo.filetype
@@ -98,30 +90,26 @@ M.show_documentation = function()
     vim.lsp.buf.hover()
   end
 end
-vim.api.nvim_set_keymap('n', 'K', ":lua require('user.keymaps').show_documentation()<CR>", opts)
+vim.keymap.set('n', 'K', ":lua require('config.maps').show_documentation()<CR>", opts)
 
--- Comment
-keymap('n', '<m-/>', "<cmd>lua require('Comment.api').toggle_current_linewise()<CR>", opts)
-keymap('x', '<m-/>', '<ESC><CMD>lua require("Comment.api").toggle_linewise_op(vim.fn.visualmode())<CR>', opts)
 
--- vim.api.nvim_set_keymap(
---   "n",
---   "<tab>",
---   "<cmd>lua require('telescope.builtin').buffers(require('telescope.themes').get_dropdown{previewer = false, initial_mode='normal'})<cr>"
---   ,
---   opts
--- )
+vim.keymap.set(
+  "n",
+  "<tab>",
+  "<cmd>lua require('telescope.builtin').buffers(require('telescope.themes').get_dropdown{previewer = false, initial_mode='normal'})<cr>",
+  opts
+)
 
 -- Tabs
-keymap('n', '<Tab>', '<cmd>BufferNext<CR>', opts)
-keymap('n', '<S-Tab>', '<cmd>BufferPrevious<CR>', opts)
-keymap('n', '<S-q>', '<cmd>BufferClose<CR>', opts)
+-- map('n', '<Tab>', '<cmd>BufferNext<CR>', opts)
+-- map('n', '<S-Tab>', '<cmd>BufferPrevious<CR>', opts)
+map('n', '<A-q>', '<cmd>BufferClose<CR>', opts)
 
 -- Easyalign
-keymap('n', 'ga', '<Plug>(EasyAlign)', opts)
-keymap('x', 'ga', '<Plug>(EasyAlign)', opts)
+map('n', 'ga', '<Plug>(EasyAlign)', opts)
+map('x', 'ga', '<Plug>(EasyAlign)', opts)
 --
 -- Browser Search
-keymap('v', '<m-s>', ":'<,'>BrowserSearch<CR>", opts)
+map('v', '<A-s>', ":'<,'>BrowserSearch<CR>", opts)
 
 return M
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -4,5 +4,24 @@ if not status then
   return
 end
 
+neotree.setup({
+  close_if_last_window = true,
+  default_component_configs = {
+    git_status = {
+      symbols = {
+        added = "✚",
+        modified = "",
+        deleted = "✖",
+        renamed = "",
+        -- Status type
+        untracked = "",
+        ignored = "",
+        unstaged = "",
+        staged = "",
+        conflict = "",
+      },
+    },
+  }
+})
 
-
+vim.keymap.set('n', '<leader>e', '<cmd>NeoTree toggle<cr>', { noremap = true, silent = true, desc = 'NeoTree Toggle'})
